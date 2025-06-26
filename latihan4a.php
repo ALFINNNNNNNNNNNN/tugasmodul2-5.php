e5dd8f895b2fcc556709febf868f2878cd9d4e48
AlpacinoSite <alfinwithirving06@gmail.com>
2025-06-26 21:48:11 +0700

    Add files via upload

--- latihan4a.php
+++ latihan4a.php
@@ -1,6 +1,14 @@
 <?php
-$array_warna = ["hijau", "kuning", "kelabu", "merah muda"];
-foreach ($array_warna as $warna) {
-    echo $warna . "<br>";
+
+$colors = array("hijau", "kuning", "kelabu", "merah", "merah muda");
+$colors[4] = "biru";
+echo "Balonku ada lima.<br>";
+echo "Rupa-rupa warnanya : <br>";
+
+foreach ($colors as $color) {
+    echo "$color, ";
 }
+echo "dan $colors[4].<br>";
+echo "meletus balon $colors[0] DOR!!!<br>";
+echo "Hatiku sangat kacau.<br>";
 ?>
